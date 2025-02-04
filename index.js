867022c584fbbaa470be4d8ac0934353948871f6
m-h-thomas <mhthomas.8193@gmail.com>
2025-02-04 11:53:38 -0700

    added DOMContentLoaded event listener to push args of addToList to HTML

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// console.log('My code is running');
-// console.log('My code is still running');
+// // console.log('My code is running');
+// // console.log('My code is still running');
 
 let listItems = []; //array to hold to do list items
 
@@ -8,4 +8,18 @@ function addToList(...task) {
 }
 
 addToList('check emails', 'complete project', 'go to gym', 'read');
+
+let alteredList = listItems.reduce((acc, item) => {
+  return acc + item
+}, "")
 console.log(listItems);
+
+console.log(alteredList);
+
+document.addEventListener('DOMContentLoaded', function() {
+  for (i = 0; i < listItems.length; i++) {
+    let li = document.createElement('li');
+    li.innerText = listItems[i];
+    myList.appendChild(li);
+  }
+})
